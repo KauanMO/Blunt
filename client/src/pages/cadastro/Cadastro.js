f4f3731b13cb1137a4dc0c5823605fe6d593e230
KauanMO <kauaanmatheus@gmail.com>
2023-10-11 23:19:53 -0300

    limpa de arquivos inuteis

---
+++ client/src/pages/cadastro/Cadastro.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios';
+import styles from './Cadastro.module.css'
+
+function Cadastro() {
+  return (
+    <div className={styles.container}>
+      
+    </div>
+  )
+}
+
+export default Cadastro
