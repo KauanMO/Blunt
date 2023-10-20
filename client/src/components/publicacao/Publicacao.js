67bd9855d2e4b9975876428db2e0dff83f85f7c4
KauanMO <kauaanmatheus@gmail.com>
2023-10-20 12:53:03 -0300

    Cores de feed e data de publicação

--- client/src/components/publicacao/Publicacao.js
+++ client/src/components/publicacao/Publicacao.js
@@ -4,6 +4,10 @@ import Imagem from '../imagem/Imagem'
 
 function Publicacao({ pubInfo }) {
     // pubInfo = dataPublicacao, fotoPerfilUsuario, fotoPublicacao, nomeExibicaoUsuario, textoPublicacao
+    let tempoPubPassado = Math.round((new Date() - new Date(pubInfo.dataPublicacao)) / (1000 * 60 * 60)) + 'h'
+    if (tempoPubPassado > 24) { tempoPubPassado /= 24 + 'd' }
+    if (tempoPubPassado > 30) { tempoPubPassado /= 30 + 'm' }
+
     return (
         <div className={styles.publicacao_container}>
             <div className={styles.topo_pub}>
@@ -14,6 +18,8 @@ function Publicacao({ pubInfo }) {
                             {pubInfo.nomeExibicaoUsuario} <span className={styles.username}>
                                 @{pubInfo.username}
                             </span>
+                            <span style={{color: 'var(--cinza)'}}>•</span>
+                            <span className={styles.dataPub}>{tempoPubPassado}</span>
                         </span>
                     </div>
                     <span>{pubInfo.textoPublicacao}</span>
