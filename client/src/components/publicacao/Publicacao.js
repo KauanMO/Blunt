a1718a3891235907b3c2c8ca75512eec9b27cec6
KauanMO <kauaanmatheus@gmail.com>
2023-10-20 09:42:16 -0300

    Apresentação de publicações sem imagem

--- client/src/components/publicacao/Publicacao.js
+++ client/src/components/publicacao/Publicacao.js
@@ -6,7 +6,21 @@ function Publicacao({ pubInfo }) {
     // pubInfo = dataPublicacao, fotoPerfilUsuario, fotoPublicacao, nomeExibicaoUsuario, textoPublicacao
     return (
         <div className={styles.publicacao_container}>
-            <Imagem imageClassName="imagem_pub" src={pubInfo.fotoPublicacao} />
+            <div className={styles.topo_pub}>
+                <Imagem imageClassName="foto_usuario_pub w_2rem h_2rem" src={pubInfo.fotoPerfilUsuario} />
+                <div className={styles.publicacao_textos}>
+                    <div className={styles.info_usuario}>
+                        <span className={styles.nome_username}>
+                            {pubInfo.nomeExibicaoUsuario} <span className={styles.username}>
+                                @{pubInfo.username}
+                            </span>
+                        </span>
+                    </div>
+                    <span>{pubInfo.textoPublicacao}</span>
+                </div>
+            </div>
+            {pubInfo.fotoPublicacao ? <Imagem imageClassName="imagem_pub as_fe" src={pubInfo.fotoPublicacao} /> : ''}
+
         </div>
     )
 }
--- server/models/publicacaoModel.js
+++ server/models/publicacaoModel.js
@@ -5,10 +5,12 @@ function publicar(textoPublicacao, fkUsuario) {
 }
 
 function buscarFeedForYou() {
-    return db.exec(`SELECT p.textoPublicacao, p.dataPublicacao, fp.fotoPublicacao, u.username, u.fotoPerfilUsuario, u.nomeExibicaoUsuario
-    FROM Publicacao p
-    JOIN fotoPublicacao fp ON fp.fkPublicacao = p.idPublicacao
-    JOIN Usuario u ON p.fkUsuario = u.idUsuario`)
+    return db.exec(`SELECT p.textoPublicacao, p.dataPublicacao, fp.fotoPublicacao, u.username, 
+    u.fotoPerfilUsuario, u.nomeExibicaoUsuario
+        FROM Publicacao p
+        LEFT JOIN fotoPublicacao fp ON fp.fkPublicacao = p.idPublicacao
+        JOIN Usuario u ON p.fkUsuario = u.idUsuario 
+        ORDER BY dataPublicacao DESC`)
 }
 
 module.exports = {
