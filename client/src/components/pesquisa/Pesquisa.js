d2ea3c0aac1b9000a392b2b7d9efe03cb1c42619
KauanMO <kauaanmatheus@gmail.com>
2023-11-24 10:53:04 -0300

    Navegação para perfil a partir de pesquisa

--- client/src/components/pesquisa/Pesquisa.js
+++ client/src/components/pesquisa/Pesquisa.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import './Pesquisa.css'
 import Imagem from '../imagem/Imagem'
+import { useNavigate } from "react-router-dom";
 
 function Pesquisa({ className, resPesquisa }) {
     //resPesquisa = idUsuario, username, fotoPerfilUsuario, nomeExibicaoUsuario
+    const navigate = useNavigate()
+
     return (
         <div className={'resultados_pesquisa_container ' + className}>
             <div className={'resultados_pesquisa'}>
                 {resPesquisa[0]
                     ? resPesquisa.map((resultado, i) => {
                         return (
-                            <div className={'resultado_pesquisa'} key={i}>
+                            <div onClick={() => { navigate(`/${resultado.username}`) }} className={'resultado_pesquisa'} key={i}>
                                 <Imagem className={'foto_perfil_pesquisa'} src={resultado.fotoPerfilUsuario} />
                                 <div className='username_nomeExib_pesquisa'>
                                     <span style={{ color: 'white' }}>{resultado.nomeExibicaoUsuario}</span>
