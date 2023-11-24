53c3075246e03c0cff8bcc66c29a316be9d7fd23
KauanMO <kauaanmatheus@gmail.com>
2023-11-24 10:50:54 -0300

    Imagem de perfil na pesquisa

--- client/src/components/pesquisa/Pesquisa.js
+++ client/src/components/pesquisa/Pesquisa.js
@@ -1,22 +1,28 @@
 import React from 'react'
 import './Pesquisa.css'
+import Imagem from '../imagem/Imagem'
 
 function Pesquisa({ className, resPesquisa }) {
+    //resPesquisa = idUsuario, username, fotoPerfilUsuario, nomeExibicaoUsuario
     return (
         <div className={'resultados_pesquisa_container ' + className}>
             <div className={'resultados_pesquisa'}>
                 {resPesquisa[0]
                     ? resPesquisa.map((resultado, i) => {
                         return (
-                            <div key={i}>
-                                {resultado.username}
+                            <div className={'resultado_pesquisa'} key={i}>
+                                <Imagem className={'foto_perfil_pesquisa'} src={resultado.fotoPerfilUsuario} />
+                                <div className='username_nomeExib_pesquisa'>
+                                    <span style={{ color: 'white' }}>{resultado.nomeExibicaoUsuario}</span>
+                                    <span style={{ color: 'var(--cinza)' }}>@{resultado.username}</span>
+                                </div>
                             </div>
                         )
                     })
                     : ''
                 }
             </div>
-        </ div>
+        </ div >
     )
 }
 
