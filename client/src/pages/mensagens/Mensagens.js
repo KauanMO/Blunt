be86e8b34de6785d25274069408669a63dc8f1dc
KauanMO <kauaanmatheus@gmail.com>
2023-11-30 15:26:55 -0300

    Começo lista de seguidores recíprocos

--- client/src/pages/mensagens/Mensagens.js
+++ client/src/pages/mensagens/Mensagens.js
@@ -2,75 +2,49 @@ import { React, useEffect, useState } from 'react'
 import styles from './Mensagens.module.css'
 import NavBar from '../../components/navbar/Navbar'
 import Rightside from '../../components/rightside/Rightside'
-import Modal from '../../components/modal/Modal'
 
 function Mensagens() {
-    const SemMensagens = () => {
-        return (
-            <div className={styles.sem_mensagens_container}>
-                <span className={styles.sem_mensagens_span}>Você não possui nenhuma mensagem.</span>
-                <span className={styles.sem_mensagens_span}>Mande uma para alguem!</span>
-                <i onClick={abrirModalPossiveisMensagens} className={`fa-regular fa-message ${styles.mandar_mensagem_icon}`}></i>
-            </div>
-        )
-    }
 
-    const SemSeguidoresReciprocos = () => {
-        return (
-            <div className={styles.texto_sem_seguidores_reciprocos}>
-                <span>Não foram encontrados seguidores recíprocos</span>
-                <i className="fa-regular fa-face-sad-tear"></i>
-            </div>
-        )
-    }
-
-    const ListaPossiveisMensagens = () => {
+    const ListaSeguidoresReciprocos = () => {
         const [seguidoresReciprocos, setSeguidoresReciprocos] = useState([])
-
         useEffect(() => {
-            const fetchSeguidoresReciprocos = async () => {
+            async function fetchSeguidoresReciprocos() {
                 const resSeguidoresReciprocos = await fetch(`/seguidores/bsr/${sessionStorage.getItem('idUsuario')}`)
                 const seguidoresReciprocos = await resSeguidoresReciprocos.json()
 
                 if (!seguidoresReciprocos[0]) {
-                    setSeguidoresReciprocos(null)
+                    setSeguidoresReciprocos([null])
                     return
                 }
-
                 setSeguidoresReciprocos(seguidoresReciprocos)
             }
             fetchSeguidoresReciprocos()
-        }, [])
+        })
 
         return (
-            <div className={styles.seguidores_reciprocos_container}>
-                {!seguidoresReciprocos
-                    ? <SemSeguidoresReciprocos />
-                    : seguidoresReciprocos.map((seguidor, i) => {
-                        return (
-                            <div key={i}>
-                                {seguidor.username}
-                            </div>
-                        )
-                    })
+            <div className={styles.lista_seguidores_reciprocos}>
+                {
+                    !seguidoresReciprocos[0]
+                        ? 'Buscando seguidores recíprocos'
+                        : seguidoresReciprocos[0] !== null
+                            ? seguidoresReciprocos.map((seguidor, i) => {
+                                return (
+                                    <div key={i}>
+                                        {seguidor.username}
+                                    </div>
+                                )
+                            })
+                            : 'Sem seguidores recíprocos'
                 }
             </div>
         )
     }
 
-    const abrirModalPossiveisMensagens = () => {
-        document.querySelector('.modal_possiveis_mensagens').style.display = 'flex'
-    }
-
     return (
         <div className={styles.mensagens_container}>
-            <Modal className={'modal_possiveis_mensagens'}>
-                <ListaPossiveisMensagens />
-            </Modal>
-
             <NavBar />
 
-            <SemMensagens />
+            <ListaSeguidoresReciprocos />
 
             <Rightside />
         </div>
